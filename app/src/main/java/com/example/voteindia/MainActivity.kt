a70bf6d38875d04d92b5bccd69605baf5fcb7945
Samarth <codewithsamarth@gmail.com>
2021-03-31 23:06:30 +0530

    Disabled Dark Theme Bug Fixed

--- app/src/main/java/com/example/voteindia/MainActivity.kt
+++ app/src/main/java/com/example/voteindia/MainActivity.kt
@@ -8,11 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Disabled Dark Theme
-
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
-        //Splash Screen For App
     }
 }
