e9847ffc2ae4655412ce5354ab32ed017de68be2
Samarth <codewithsamarth@gmail.com>
2021-04-01 11:53:25 +0530

    Added All the components fixed Bugs and created OTP Screen Added

--- app/src/main/java/com/example/voteindia/SplashScreen.kt
+++ app/src/main/java/com/example/voteindia/SplashScreen.kt
@@ -18,9 +18,6 @@ class SplashScreen : AppCompatActivity() {
 
     fun getSplash()
     {
-        //Setting Status Bar Color
-
-        window.statusBarColor = ContextCompat.getColor(this, R.color.orangecustom)
 
         // Getting the Views From Xml
 
@@ -54,7 +51,7 @@ class SplashScreen : AppCompatActivity() {
 
         // Intent Switcher
         Handler(Looper.getMainLooper()).postDelayed({
-            val i = Intent(this,MainActivity::class.java)
+            val i = Intent(this,otpScreen::class.java)
             startActivity(i)
             finish()
         },3800)
---
+++ app/src/main/java/com/example/voteindia/otpScreen.kt
@@ -0,0 +1,11 @@
+package com.example.voteindia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class otpScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp_screen)
+    }
+}
